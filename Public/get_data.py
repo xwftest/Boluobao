0b7a35fe646e4869953b00855f6ab3c091aa3347
xiaofeng <1532456560@qq.com>
2023-03-26 14:25:37 +0800

    提交data管理数据

---
+++ Public/get_data.py
@@ -0,0 +1,21 @@
+import os
+import openpyxl
+
+
+def getData(row_num, col_num):
+
+    # 打开表文件
+    workbook = openpyxl.load_workbook(os.path.abspath(os.path.join(os.getcwd(),
+                                                                   "../Data/data.xlsx")))
+    # 选中要读取的表
+    sheet = workbook['Sheet1']
+    # 读取指定行号和列号的内容
+    value = sheet.cell(row=row_num, column=col_num).value
+    # 关闭表文件
+    workbook.close()
+    return value
+
+
+if __name__ == "__main__":
+    value = getData(1,2)
+    print(value)
--- TestCase/test_Login.py
+++ TestCase/test_Login.py
@@ -6,6 +6,7 @@ from Page.protocol_popup_page import ProtocolPopupPage
 from Page.sign_up_page import SignUpPage
 from Public.base_driver import init_driver
 from time import sleep
+from Public.get_data import getData
 
 
 class TestLogin(unittest.TestCase):
@@ -18,14 +19,17 @@ class TestLogin(unittest.TestCase):
 
     def test_login(self):
         self.protocol_popup_page.agree_click()
-        sleep(2)
+        sleep(3)
         # 跳转我的页面
         self.driver.find_element(By.ID, 'com.sfacg:id/main_tab_container5').click()
         sleep(2)
         self.my_page.click_signin()
-        sleep(2)
-        self.sign_up_page.account_input('hexid')
-        self.sign_up_page.password_input('486456')
+        # 获取登录账号和密码
+        name = getData(1, 2)
+        pwd = getData(1, 3)
+        sleep(3)
+        self.sign_up_page.account_input(number=name)
+        self.sign_up_page.password_input(password=pwd)
         self.sign_up_page.login_click()
         sleep(1)
         self.sign_up_page.agreement()
